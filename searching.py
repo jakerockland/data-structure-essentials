cf2bacd8d9852bc3b8f64b7a9bdbf23d116b7799
Jake Rockland <jakerockland@email.arizona.edu>
2015-11-09 01:24:53 -0700

    changed naming convention

--- searching.py
+++ searching.py
@@ -2,26 +2,26 @@
 # By: Jacob Rockland
 
 # Linear search is an O(n) search algorithm
-def linear_search(numbers, key):
-    for number in numbers:
-        if number == key:
-            return number
+def linear_search(items, key):
+    for item in items:
+        if item == key:
+            return item
 
     return None # element not found
 
 # Binary search is an O(log(n)) search algorithm
-def binary_search(numbers, key):
+def binary_search(items, key):
     low = 0
-    high = len(numbers) - 1
+    high = len(items) - 1
     mid = 0
 
     while high >= low:
         mid = (high + low) / 2
-        if key > numbers[mid]:
+        if key > items[mid]:
             low = mid + 1
-        elif key < numbers[mid]:
+        elif key < items[mid]:
             high = mid - 1
         else:
-            return numbers[mid]
+            return items[mid]
 
     return None # element not found
